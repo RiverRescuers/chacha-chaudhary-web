3ccc13ddd44eb08cd921cbf2f32cd4b641dc0f32
SUKHAD <mykeyman777@gmail.com>
2023-09-27 18:07:50 +0530

    message

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,22 +4,23 @@ import Name from "../assets/images/NMCG name.png";
 import FLogo from "../assets/images/Full Logo.png";
 import CCHead from "../assets/images/cc head.png";
 import Formula from "../assets/images/formula.gif";
+import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <div>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
             {/* <img src={NavLogo} alt="" /> */}
             {/* <img src={Name} alt="" /> */}
             <img src={FLogo} alt="NMCG" />
             {/* <img src={CCHead} alt="NMCG" /> */}
             {/* <img src={Formula} alt="NMCG" /> */}
-          </a>
+          </Link>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -27,58 +28,63 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul
-              class="navbar-nav me-auto mb-2 mb-lg-0 gap-4"
+              className="navbar-nav me-auto mb-2 mb-lg-0 gap-4"
               style={{ marginLeft: "60px" }}
             >
-              <li class="nav-item">
-                <a
-                  class="nav-link active with-border"
-                  style={{ color: "skyblue" }}
+              <li className="nav-item">
+                <Link
+                  to="/chatbot"
+                  className="nav-link active with-border"
+                  style={{ color: "crimson" }}
                   aria-current="page"
                   href="#"
                 >
                   Chacha Chaudary
-                </a>
+                </Link>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link with-border"
-                  style={{ color: "skyblue" }}
+              <li className="nav-item">
+                <Link
+                  to="/api"
+                  className="nav-link with-border"
+                  style={{ color: "crimson" }}
                   href="#"
                 >
                   Programs & Initiatives
-                </a>
+                </Link>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link with-border"
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  className="nav-link with-border"
                   style={{ color: "crimson" }}
                   href="#"
                 >
                   River Ecology
-                </a>
+                </Link>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link with-border"
+              <li className="nav-item">
+                <Link
+                  to="/gallery"
+                  className="nav-link with-border"
                   style={{ color: "crimson" }}
                   href="#"
                 >
                   Gallery
-                </a>
+                </Link>
               </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link with-border"
+              <li className="nav-item">
+                <Link
+                  to="/"
+                  className="nav-link with-border"
                   style={{ color: "crimson" }}
                   href="#"
                 >
                   About Us
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
