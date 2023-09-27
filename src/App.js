e7226e3ce8851f09adbdfdb431e6289412e67e93
Vy <vyom.j@protonmail.com>
2023-09-28 01:06:10 +0530

    fix: conflicts

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import ChatBot from "./components/ChatBot";
-import Api from "./components/Api";
 import Gallery from "./components/Gallery";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Main />} />
           <Route exact path="/chatbot" element={<ChatBot />} />
-          <Route exact path="/api" element={<Api />} />
           <Route exact path="/gallery" element={<Gallery />} />
         </Routes>
       </Router>
